af29e2fbef4694dd2f974d4548052eedb92c9d11
Matej <matej.zvezdakoski@gmail.com>
2026-06-10 21:39:23 +0200

    initialCommit

---
+++ Proekt VP/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proekt_VP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content= new MainMenuView();
+        }
+        public void ChangeView(UserControl view)
+        {
+            MainContent.Content = view;
+        }
+
+        
+    }
+}
